d0cf1d7daab0aed7c7a03481fb4067962ed93778
Rajdeep <rajdeepchoudhary16@gmail.com>
2025-11-04 16:33:45 +0530

    main.py added

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+# todo_project/
+# │
+# ├── backend/
+# │   ├── main.py
+# │   ├── database.py
+# │   ├── models.py
+# │
+# │   ├── auth/
+# │   │   ├── routes.py
+# │   │   ├── password_hashing.py
+# │   │   └── jwt_handler.py
+# │
+# │   ├── todo/
+# │   │   └── routes.py
+# │
+# ├── requirements.txt
+# └── README.me 
